70500fdbb4e052f8fafa0383fb9c14ac4ae3466d
Rudolf Muehlbauer <rudolf.muehlbauer@gmail.om>
2014-11-15 21:16:33 +0100

    upd

--- test/from_image.sh
+++ test/from_image.sh
@@ -1,6 +1,8 @@
 #! /bin/bash -xeu
 
-../src/crane_gps_watch_client --out output.tcx --from_image image.bin
+pwd
+
+${srcdir}/crane_gps_watch_client --out output.tcx --from_image image.bin
 
 xmllint --schema TrainingCenterDatabasev2.xsd --noout output.tcx
 
