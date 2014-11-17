f94cddab0b70c558e16ae1eb05f68fe6d17e1245
Rudolf Muehlbauer <rudolf.muehlbauer@gmail.om>
2014-11-17 11:14:51 +0100

    reduce test 008

--- test/008_lint_images.sh
+++ test/008_lint_images.sh
@@ -4,7 +4,7 @@ pwd
 
 for img in ${srcdir}/lint_images/*.bin; do
 
-  ../src/crane_gps_watch_client --lint --verbose --verbose --out 008_output.tcx --from_image $img
+  ../src/crane_gps_watch_client --lint --verbose --out 008_output.tcx --from_image $img
   xmllint --schema ${srcdir}/TrainingCenterDatabasev2.xsd --noout 008_output.tcx
 done
 
