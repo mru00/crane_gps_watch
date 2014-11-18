d729f0160e0d6db5082a0f1cd6ae4e9383169316
Rudolf Muehlbauer <rudolf.muehlbauer@gmail.om>
2014-11-18 23:25:11 +0100

    fixed testcase

--- test/009_expect_coordinates.sh
+++ test/009_expect_coordinates.sh
@@ -2,5 +2,5 @@
 
 ../src/crane_gps_watch_client --from_image $srcdir/knedlyk_fail.bin --verbose > 009_log
 
-grep ' workout end, samples with coord: 9000' 009_log
+grep ' workout end samples with coord=9000' 009_log
 
